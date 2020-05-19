d441e5cb83ef03126237deeacf68b8a12655481f
LyfeTyme <48641165+LyfeTyme@users.noreply.github.com>
2020-05-19 12:37:36 -0600

    Final Todo
    added todo app
    
    need to add links for quick nav

---
+++ js/main.js
@@ -0,0 +1,119 @@
+// SELECT ELEMENT
+
+const clear = document.querySelector(".clear");
+const dateElement = document.getElementById("date");
+const list = document.getElementById("list");
+const input = document.getElementById("input");
+
+// CLASS NAMES
+const CHECK = "fa-check-circle";
+const UNCHECK = "fa-circle-thin";
+const LINE_THROUGH = "lineThrough";
+
+let LIST, id;
+
+
+
+// get item from localestorage
+let data = localStorage.getItem("TODO");
+
+if(data){
+    LIST = JSON.parse(data);
+    id = LIST.length;
+    loadList(LIST);
+}else{
+    LIST = [];
+    id = 0;
+}
+
+// load items to interface
+function loadList(array){
+    array.forEach(function(item){
+        addToDo(item.name, item.id, item.done, item.trash);
+    });
+}
+
+clear.addEventListener("click", function(){
+    localStorage.clear();
+    location.reload();
+})
+// SHOW TODAY DATE
+const options = {weekday: "long", month : "short", day : "numeric"};
+const today = new Date();
+
+dateElement.innerHTML = today.toLocaleDateString("en-US", options);
+
+// TO FUNCTION
+
+function addToDo(toDo, id, done, trash){
+
+    if(trash){
+        return;
+
+    }
+    const DONE = done ? CHECK : UNCHECK;
+    const LINE = done ? LINE_THROUGH : "";
+    const item = `
+                    <li class="item">
+                    <i class="fa ${DONE} co" job="complete" id="${id}"></i>
+                    <p class="text ${LINE}">${toDo}</p>
+                    <i class="fa fa-trash-o de" job="delete" id="${id}"></i>
+                    </li>
+                `;
+    const position = "beforeend";
+
+    list.insertAdjacentHTML(position, item);
+}
+
+document.addEventListener("keyup", function(even){
+    if(even.keyCode == 13){
+        const toDo = input.value;
+        if(toDo){
+            addToDo(toDo, id, false, false);
+
+            LIST.push({
+                name: toDo,
+                id: id,
+                done: false,
+                trash: false
+            });
+
+            localStorage.setItem("TODO", JSON.stringify(LIST));
+            id++;
+        }
+        input.value = "";
+    }
+});
+
+// addToDo("Coffee", 1, false, false);
+
+// completion on todo
+function completeToDo(element){
+    element.classList.toggle(CHECK);
+    element.classList.toggle(UNCHECK);
+    element.parentNode.querySelector(".text").classList.toggle(LINE_THROUGH);
+
+    LIST[element.id].done = LIST[element.id].done ? false : true;
+}
+
+// remove to do
+function removeToDo (element){
+    element.parentNode.parentNode.removeChild(element.parentNode);
+
+    LIST[element.id].trash = true;
+}
+
+// TARGET DYNA OBJ
+
+list.addEventListener("click", function(event){
+    const element = event.target;
+    const elementJob = element.attributes.job.value;
+
+    if(elementJob == "complete"){
+        completeToDo(element);
+    }else if(elementJob == "delete"){
+        removeToDo(element);
+
+    }
+    localStorage.setItem("TODO", JSON.stringify(LIST));
+});
